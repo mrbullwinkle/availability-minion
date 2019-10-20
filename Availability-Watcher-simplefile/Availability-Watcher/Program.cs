aa83150f6376f0f622e4553d103420acb454d265
CodeBeforeWork <codebeforework@users.noreply.github.com>
2019-10-20 16:44:23 -0400

    Sample

---
+++ Availability-Watcher-simplefile/Availability-Watcher/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace Availability_Watcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                
+                });
+    }
+}
